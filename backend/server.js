afd2ee1166b2b941214f8be866e99df67c4e3cb5
spartanHarvey <jorgenathan257@gmail.com>
2021-12-01 09:01:13 -0600

    added new endpoints

--- backend/server.js
+++ backend/server.js
@@ -32,29 +32,8 @@ app.use(cors())
 
 
 
-// ALTER TABLE paymentdetails DROP FOREIGN KEY customerID
-// ALTER TABLE reservationdetails DROP FOREIGN KEY customerID
 
-// con.query("ALTER TABLE reservationdetails DROP FOREIGN KEY id, drop id", function (err, result) {
-//     if (err) throw err;
-//     console.log("Table created");
-//   });
-// var sql = `CREATE TABLE IF NOT EXISTS customers  
-
-//         (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-//          firstname VARCHAR(255) NOT NULL,
-//password VARCHAR(255) NOT NULL,
-//         lastName VARCHAR(255) NOT NULL,
-//         email VARCHAR(255) NOT NULL UNIQUE,
-//         mailAddress VARCHAR(255) NOT NULL,
-//         billAddress VARCHAR(255) NOT NULL,
-//         phoneNumber VARCHAR(255) NOT NULL),
-//         earnedPoints INT DEFAULT 0`;
-// con.query(sql, function (err, result) {
-//   if (err) throw err;
-//   console.log("Table created");
-// });
-
+// printtable()
 function printtable(){
 
     con.query("SELECT * FROM guests", function (err, result, fields) {
@@ -65,11 +44,13 @@ function printtable(){
         if (err) throw err;
         console.log(result);
        });
-       con.query("SELECT * FROM paymentdetails", function (err, result, fields) {
+       con.query("SELECT * FROM payments", function (err, result, fields) {
         if (err) throw err;
+
+
         console.log(result);
        });
-       con.query("SELECT * FROM reservationdetails", function (err, result, fields) {
+       con.query("SELECT * FROM reservations", function (err, result, fields) {
         if (err) throw err;
         console.log(result);
        });
@@ -83,50 +64,48 @@ function droptable(name){
     var sql = `DROP TABLE IF EXISTS  ${name}`;
     con.query(sql, function (err, result) {
       if (err) throw err;
-      console.log("Table deleted");
+      console.log(`Table ${name} deleted`);
     });
   
 
 }
-function createtablecustomers(){
 
 
-    var sql = `CREATE TABLE IF NOT EXISTS customers  
+// function createtablecustomers(){
+
+
+//     var sql = `CREATE TABLE IF NOT EXISTS customers  
     
-            (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-             firstname VARCHAR(255) NOT NULL,
-            password VARCHAR(255) NOT NULL,
-            lastName VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL UNIQUE,
-            mailAddress VARCHAR(255) NOT NULL,
-            billAddress VARCHAR(255) NOT NULL,
-            phoneNumber VARCHAR(255) NOT NULL),
-            earnedPoints INT DEFAULT 0`;
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
-}
+//             (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//              firstname VARCHAR(255) NOT NULL,
+//             password VARCHAR(255) NOT NULL,
+//             lastName VARCHAR(255) NOT NULL,
+//             email VARCHAR(255) NOT NULL UNIQUE,
+//             mailAddress VARCHAR(255) NOT NULL,
+//             billAddress VARCHAR(255) NOT NULL,
+//             phoneNumber VARCHAR(255) NOT NULL),
+//             earnedPoints INT DEFAULT 0`;
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Table created");
+//     });
+// }
 
-
-// FOREIGN KEY (reservationID) REFERENCES cus(PersonID)
-
-
-function createtablereservation(){
- var sql = `CREATE TABLE IF NOT EXISTS reservationdetails
-            (id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            customerID INT,
-            guestNumber INT ,
-            paymentID INT, 
-            date DATE NOT NULL,
-            time DATETIME NOT NULL,
-            FOREIGN KEY (customerID) REFERENCES customers(id),
-            FOREIGN KEY (paymentID) REFERENCES paymentdetails(id))`;
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
-}
+// function createtablereservation(){
+//  var sql = `CREATE TABLE IF NOT EXISTS reservationdetails
+//             (id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//             customerID INT,
+//             guestNumber INT ,
+//             paymentID INT, 
+//             date DATE NOT NULL,
+//             time DATETIME NOT NULL,
+//             FOREIGN KEY (customerID) REFERENCES customers(id),
+//             FOREIGN KEY (paymentID) REFERENCES paymentdetails(id))`;
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Table created");
+//     });
+// }
 
 //  var sql = `CREATE TABLE IF NOT EXISTS paymentdetails
 //             (id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -160,19 +139,9 @@ function describetable(table){
       console.log(result);
     });
 
-// var sql = "DESCRIBE paymentdetails";
-// con.query(sql, function (err, result) {
-//   if (err) throw err;
-//   console.log(result);
-// });
+    
+}
 
-//     var sql = `INSERT INTO customers 
-// (firstName, lastName, email, mailAddress,billAdress,phoneNumber) 
-// VALUES ('Hammer','Doll','hammerd@email.com','6969 thatway st', '6969 thatway st','11111111')`;
-//   con.query(sql, function (err, result) {
-//     if (err) throw err;
-//     console.log("1 record inserted");
-//   });
 
 function showtable (){
 
@@ -185,9 +154,121 @@ function showtable (){
 
 }
 
-  
 
-app.get('/user/:id', cors(), async (req, res) => {
+app.post('/registration', cors(), async (req, res) => {
+
+    // Input name's should be named the same as below
+    const { firstName, lastName, email, password, mailAddress, billAddress, phoneNumber } = req.body
+
+
+    var sql = `SELECT email FROM customers WHERE email='${email}'`
+
+    con.query(sql, function (err, result, fields) {
+        if (err) throw err;
+        if(result.length > 0) return res.status(403).send({"error":"email already taken"});            
+       
+        var sql = `INSERT INTO customers (firstname,lastName,email,password,mailAddress,billAddress,phoneNumber) 
+        VALUES ('${firstName}','${lastName}','${email}','${password}','${mailAddress}','${billAddress}','${phoneNumber}')`;
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log(result[0] + "1 record inserted");
+            res.send(result)
+        });
+
+    });
+
+app.post('/login', cors(), async (req, res) => {
+
+        const { email, password } = req.body;
+        var sql = `SELECT * FROM customers WHERE email='${email}'`
+
+        con.query(sql, function (err, result, fields) {
+            if (err) throw err;
+
+            if (result.length === 0) res.send({'error':' email not registered'});
+
+            if (password === result[0].password) {
+
+                req.session.currentUser = result[0].id;
+                res.send(result)
+            }
+
+
+        });
+
+
+    });
+
+
+});
+
+
+app.delete('/customers/:id', cors(), async (req, res) => {
+
+
+        /*
+          deletes registed customer based on id which is passed in the 
+          url query string we will change to req.session.customer.id
+        
+        
+        */
+        var sql = `SELECT * FROM customers WHERE id='${req.params.id}'`
+
+        con.query(sql, function (err, result, fields) {
+            if (err) throw err;
+
+            if (result.length === 0) res.send({'error':' email not registered'});
+
+            if (password === result[0].password) {
+
+                req.session.currentUser = result[0].id;
+                res.send(req.session)
+            }
+
+
+        });
+
+
+});
+
+
+
+/*
+    should return a single guests from the db
+    based on id
+*/
+app.get('/guests/:id', cors(), async (req, res) => {
+
+    // if(req.params.id != req.session.currentUser && !req.session.authenticated) return res.send('Login first')
+    con.query(`SELECT * FROM guests WHERE id ='${req.params.id}'`, function (err, result, fields) {
+        if (err) throw err;
+
+        res.send(result);
+    });
+
+
+});
+
+/*
+    should return all the guests in the db
+*/
+app.get('/guests', cors(), async (req, res) => {
+
+    con.query("SELECT * FROM guests", function (err, result, fields) {
+        if (err) throw err;
+
+        res.send(result);
+    });
+
+
+});
+
+/*
+    should return a specific registered customer from the db
+    based on id
+*/
+
+app.get('/customers/:id', cors(), async (req, res) => {
 
     // if(req.params.id != req.session.currentUser && !req.session.authenticated) return res.send('Login first')
     con.query(`SELECT * FROM customers WHERE id ='${req.params.id}'`, function (err, result, fields) {
@@ -198,8 +279,28 @@ app.get('/user/:id', cors(), async (req, res) => {
 
 
 });
-app.put('/user/:id', cors(), async (req, res) => {
+/*
+    should return all the registered customers in the db
+*/
+app.get('/customers', cors(), async (req, res) => {
 
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+        if (err) throw err;
+
+        res.send(result);
+    });
+
+
+});
+
+app.put('/customers/:id', cors(), async (req, res) => {
+
+
+     /*  
+         A valid put request to this route will update a registered
+         user information in the db then return the updated result
+     
+      */
     // if(!req.params.id == req.session.id && req.session.authenticated) return res.send('Login first')
     // const {firstName, lastName,email,password,mailAddress,billAddress,phoneNumber} = req.body
     var sql = `SELECT * FROM customers WHERE id='${req.params.id}'`
@@ -222,67 +323,6 @@ app.put('/user/:id', cors(), async (req, res) => {
     });
 
 });
-
-
-app.post('/login', cors(), async (req, res) => {
-
-    const { email, password } = req.body;
-    var sql = `SELECT * FROM customers WHERE email='${email}'`
-
-    con.query(sql, function (err, result, fields) {
-        if (err) throw err;
-
-        if (result.length === 0) res.send('wrong email');
-
-        if (password === result[0].password) {
-
-            req.session.authenticated = true;
-            req.session.currentUser = result[0].id;
-            res.send(req.session)
-        }
-
-
-    });
-
-
-});
-
-app.post('/registration', cors(), async (req, res) => {
-
-    // Input name's should be named the same as below
-    const { firstName, lastName, email, password, mailAddress, billAddress, phoneNumber } = req.body
-
-
-    var sql = `SELECT email FROM customers WHERE email='${email}'`
-
-    con.query(sql, function (err, result, fields) {
-        if (err) throw err;
-        if(result.length > 0) return;            
-       
-        var sql = `INSERT INTO customers (firstname,lastName,email,password,mailAddress,billAddress,phoneNumber) 
-        VALUES ('${firstName}','${lastName}','${email}','${password}','${mailAddress}','${billAddress}','${phoneNumber}')`;
-        con.query(sql, function (err, result) {
-            if (err) throw err;
-            console.log(result + "1 record inserted");
-            res.send(result)
-        });
-
-    });
-
-
-
-});
-app.get('/users', cors(), async (req, res) => {
-
-    con.query("SELECT * FROM customers", function (err, result, fields) {
-        if (err) throw err;
-
-        res.send(result);
-    });
-
-
-});
-
 // table reservation routes
 
 //  var sql = `CREATE TABLE IF NOT EXISTS reservationdetails
@@ -298,25 +338,44 @@ app.get('/users', cors(), async (req, res) => {
 //       if (err) throw err;
 //       console.log("Table created");
 //     });
+app.post('/checktime', cors(), async(req,res) =>{
 
+    const { date , numberOfGuests} = req.body;
+
+    /// table availableTable -> number of tables | number of seats  | date of reservation
+    // return to frontend all the # of seats available on a specific date
+
+
+
+})
 
 app.post('/reservation', cors(), async(req,res)=>{
 
     // if(!req.body) res.send("empty fields")
+    console.log(req.body )
     const {firstName, lastName,email, phoneNumber, cardNumber,securityCode,guestNumber,date,time } = req.body;
 
+
+    /*  should query the db to find an so that we know
+         if the user making the reservation is registered or not.
+         if it is then we will store into reservation db with
+         customerID otherwise use guestsID
+
+    
+    
+    */
     // var sql =`SELECT email FROM customers WHERE email='${email}'`
            
     // con.query(sql, function (err, result, fields) {
     //     if (err) throw err;
     //     if(result.length > 0) return;            
     //    });  
-        // var sql = `INSERT INTO reservationdetails VALUES ?`;
-        // con.query(sql,req.body, function (err, result) {
-        //     if (err) throw err.message;
-        //         console.log(result + "1 record inserted");
-        //         res.send(result)
-        //     });
+    // var sql = `INSERT INTO reservationdetails VALUES ?`;
+    // con.query(sql,req.body, function (err, result) {
+    //     if (err) throw err.message;
+    //         console.log(result + "1 record inserted");
+    //         res.send(result)
+    //     });
 
         var sql = `INSERT INTO guests (firstname,lastName,email,phoneNumber) 
         VALUES ('${firstName}','${lastName}','${email}','${phoneNumber}')`;
@@ -326,20 +385,20 @@ app.post('/reservation', cors(), async(req,res)=>{
             console.log(resultGuest + "1  guests inserted");
                 // res.send(result)
 
-            var sql = `INSERT INTO paymentdetails (customerID,cardNumber,securityCode) 
+            var sql = `INSERT INTO payments (customerID,cardNumber,securityCode) 
             VALUES ('${resultGuest.insertedId}','${cardNumber}','${securityCode}')`;
             con.query(sql, function (err, result) {
             if (err) throw err.message;
-            console.log(result + "1 paymentdetails inserted");
+            console.log(result + "1 payments inserted");
                         // res.send(result)
                    
                 
-                    var sql = `INSERT INTO reservationdetails (customerID,guestNumber,paymentID,date,time) 
-                    VALUES ('${resultGuest,insertedId}','${guestNumber}','${result.insertedId}','${date}','${time}')`;
+                    var sql = `INSERT INTO reservations (customerID,guestNumber,paymentID,date,time) 
+                    VALUES ('${resultGuest.insertedId}','${guestNumber}','${result.insertedId}','${date}','${time}')`;
 
                     con.query(sql, function (err, result) {
                         if (err) throw err.message;
-                            console.log(result + "1 reservationdetails inserted");
+                            console.log(result + "1 reservations inserted");
                             res.send(result)
                     
                 
